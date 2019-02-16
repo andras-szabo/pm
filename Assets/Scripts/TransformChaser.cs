00bfd52848f954edf589ee46e96fa89499198326
andras <andras.szabo.fin@gmail.com>
2019-02-16 22:13:14 +0100

    First stab at homing missile logic (TransformChaser)

---
+++ Assets/Scripts/TransformChaser.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class TransformChaser : MonoWithCachedTransform
+{
+	private Transform _target;
+
+	public void Setup(Transform target)
+	{
+		_target = target;
+	}
+
+	[Range(30f, 180f)]
+	public float maxRotationAnglesPerSec;
+
+	[Range(1f, 100f)]
+	public float speedPerSecond;
+
+	private void Update()
+	{
+		if (_target != null)
+		{
+			var toTarget = (_target.position - CachedTransform.position).normalized;
+			var angleDifference = Vector3.Angle(CachedTransform.forward, toTarget);
+
+			if (angleDifference > 1f)
+			{
+				RotateTowards(toTarget, angleDifference);
+			}
+
+			CachedTransform.position += CachedTransform.forward * speedPerSecond * Time.deltaTime;
+		}
+	}
+
+	private void RotateTowards(Vector3 toTarget, float angleDifference)
+	{
+		var maxRotationPerFrame = Time.deltaTime * maxRotationAnglesPerSec;
+		var slerpFactor = angleDifference > maxRotationPerFrame ? maxRotationPerFrame / angleDifference :
+																  angleDifference / maxRotationPerFrame;
+
+		CachedTransform.rotation = Quaternion.Slerp(CachedTransform.rotation,
+													Quaternion.LookRotation(toTarget, Vector3.up),
+													slerpFactor);
+	}
+}
---
+++ Assets/Scripts/TransformChaserTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformChaserTest : MonoBehaviour
+{
+	public Transform target;
+	
+	private void Start()
+	{
+		var chaser = GetComponent<TransformChaser>();
+		chaser.Setup(target);
+	}
+}
