df998dffa4bea733c865f53c5a414cd49d6b1da2
andras <andras.szabo.fin@gmail.com>
2019-02-17 00:19:19 +0100

    Small cleanup.

--- Assets/Scripts/DestroyOnImpact.cs
+++ Assets/Scripts/DestroyOnImpact.cs
@@ -8,13 +8,5 @@ public class DestroyOnImpact : MonoBehaviour
 	private void OnCollisionEnter(Collision collision)
 	{
 		Destroy(this.gameObject);
-		
-		// OK how about this.
-		// on collision, I talk to the collision system:
-		// I hit collision.transform, dealing them X damage, such and such arguments.
-		// And then I destroy myself.
-		// Meanwhile, collidable dudes register with the CollisionManager, with their transform.
-		// So the collisionManager can disambiguate.
-		// Let's do this.
 	}
 }
