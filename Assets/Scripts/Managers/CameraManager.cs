fecaaf2b3ed5464428e384b30f45674b34f77a23
andras <andras.szabo.fin@gmail.com>
2019-02-17 07:02:49 +0100

    More cleanup; started with ICameraManager.

---
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour, ICameraManager
+{
+	private void Awake()
+	{
+		GameController.TryRegister<ICameraManager>(this);	
+	}
+}
